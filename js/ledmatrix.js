df32db1904ab92a2daa01d903df22cd6e03a94be
Hubert Dąbrowski <hubsdab@gmail.com>
2021-05-19 02:05:34 +0200

    updated

--- js/ledmatrix.js
+++ js/ledmatrix.js
@@ -117,66 +117,6 @@ function init() {
 		initDisplay(responseJSON);
 	}
 	});
-	// $.get("get_pixels.php", function(data, status){
-		// var LEDlist = data;
-		// var row;
-		// var col;
-		// var counter = 0;
-		// var row = $("<div>",{"class":"row"});
-		// for (var i=0; i < 8; i++) {
-			// for(var j=0; j< 8; j++) {
-				// var RGBarray = LEDlist[counter];
-				// var R = RGBarray[0];
-				// var G = RGBarray[1];
-				// var B = RGBarray[2];
-				// var colorcode = "#"+R.toString(16).padStart(2,'0')+G.toString(16).padStart(2,'0')+B.toString(16).padStart(2,'0')
-
-				// var color_input = $("<input>",{
-					// id: "led"+i+j, 
-					// type: "button",
-					// "class":"btn btn-default form-control"
-				// });
-				// color_input.css("background-color", colorcode);
-				// color_input.css("border", "1px solid #000000");
-				// color_input.click(changeColor);
-				// color_input.change(function(){
-					// var led_id = $(this).attr('id').split('_')[2];
-					// var led_x = led_id % 8;
-					// var led_y = Math.floor(led_id/8);
-					// var color_hex_text = $(this).val();
-					// var color_integer = parseInt(color_hex_text.substring(1), 16);
-					// var R = (color_integer & 0xff0000) >> 16;
-					// var G = (color_integer & 0x00ff00) >> 8;
-					// var B = color_integer & 0x0000ff;
-					// var url = "set_pixel.php?x="+led_x+"&y="+led_y+"&r="+R+"&g="+G+"&b="+B;						
-					// $.get(url, function(data, status){	        
-						// $("#response").text(data);							
-					// })  .done(function(data) {
-						// $("#response").text("Response: "+data);
-					  // })
-					  // .fail(function() {
-						// $("#response").html("Request with error. Check connection.<br>");
-					  // })
-					  // .always(function() {
-						// $("#request").html(url);
-					  // });
-				// });
-				
-				
-				// var col = $("<div>",{"class":"col", "id":"led_div_"+i+j});					
-				// var input_group = $("<div>",{"class":"input-group"});
-			
-				// input_group.append(color_input);
-				// col.append(input_group);
-				// row.append(col);
-				// $("#led_matrix").append(row);	
-				// counter++;
-			// }
-			// var row = $("<div>",{"class":"row pad_top"});
-		// }
-	// }, "json").fail(function() {
-	// console.log("Fail of HTTP GET request.");
-	// });
 };
 
 $(document).ready(() => { 
